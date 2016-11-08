a3ba2bf4d841aa237ec35642fcc02168151cf8b4
emilio <emilio.dorigatti@gmail.com>
2016-11-08 18:28:51 +0100

    computing sentiment over time

--- classifier/graph.py
+++ classifier/graph.py
@@ -1,217 +1,243 @@
 import json
+import multiprocessing as mp
+import os
+import click
 import datetime
 import sentiment
+import pickle
 from matplotlib import pyplot as plt
 
 
-base_date = datetime.datetime(3000,12,31,12,12,12)
-upper_date = datetime.datetime(1000,01,01,00,00,00)
+def get_sentiment((input_file, classifier)):
+    base_date = datetime.datetime(3000,12,31,12,12,12)
+    upper_date = datetime.datetime(1000,01,01,00,00,00)
 
-sayac = 0
-with open('tweets.jsonl') as f:
-	for row in f:
-		tweet = json.loads(row)
-		sayac = sayac + 1
-			
-		temp_date = tweet['_source']['created_at']
-			
-		temp_date = temp_date.split(" ")
-		temp_year = int(temp_date[5])
-			
-		if temp_date[1] == "Jan":
-			temp_month = 1
-		if temp_date[1] == "Feb":
-			temp_month = 2
-		if temp_date[1] == "Mar":
-			temp_month = 3
-		if temp_date[1] == "Apr":
-			temp_month = 4
-		if temp_date[1] == "May":
-			temp_month = 5
-		if temp_date[1] == "Jun":
-			temp_month = 6
-		if temp_date[1] == "Jul":
-			temp_month = 7
-		if temp_date[1] == "Aug":
-			temp_month = 8
-		if temp_date[1] == "Sep":
-			temp_month = 9
-		if temp_date[1] == "Oct":
-			temp_month = 10
-		if temp_date[1] == "Nov":
-			temp_month = 11
-		if temp_date[1] == "Dec":
-			temp_month = 12	
-			
+    sayac = 0
+    with open(input_file) as f:
+        for row in f:
+            tweet = json.loads(row)
+            sayac = sayac + 1
+                
+            temp_date = tweet['_source']['created_at']
+                
+            temp_date = temp_date.split(" ")
+            temp_year = int(temp_date[5])
+                
+            if temp_date[1] == "Jan":
+                temp_month = 1
+            if temp_date[1] == "Feb":
+                temp_month = 2
+            if temp_date[1] == "Mar":
+                temp_month = 3
+            if temp_date[1] == "Apr":
+                temp_month = 4
+            if temp_date[1] == "May":
+                temp_month = 5
+            if temp_date[1] == "Jun":
+                temp_month = 6
+            if temp_date[1] == "Jul":
+                temp_month = 7
+            if temp_date[1] == "Aug":
+                temp_month = 8
+            if temp_date[1] == "Sep":
+                temp_month = 9
+            if temp_date[1] == "Oct":
+                temp_month = 10
+            if temp_date[1] == "Nov":
+                temp_month = 11
+            if temp_date[1] == "Dec":
+                temp_month = 12	
+                
 
-		temp_day = int(temp_date[2])
+            temp_day = int(temp_date[2])
 
-		temp_hour = int(temp_date[3][0] + temp_date[3][1])
-		temp_min = int(temp_date[3][3] + temp_date[3][4])
-		temp_sec = int(temp_date[3][6] + temp_date[3][7]) 
+            temp_hour = int(temp_date[3][0] + temp_date[3][1])
+            temp_min = int(temp_date[3][3] + temp_date[3][4])
+            temp_sec = int(temp_date[3][6] + temp_date[3][7]) 
 
-		date = datetime.datetime(temp_year,temp_month,temp_day,temp_hour,temp_min,temp_sec)
-		
-		if date < base_date:
-			base_date = date
-		if date > upper_date:
-			upper_date = date
-		
+            date = datetime.datetime(temp_year,temp_month,temp_day,temp_hour,temp_min,temp_sec)
+            
+            if date < base_date:
+                base_date = date
+            if date > upper_date:
+                upper_date = date
+            
 
-	print base_date
-	print upper_date
+        print base_date
+        print upper_date
 
-	#find number of bins using time difference
-	t_diff = upper_date-base_date
-	print t_diff
-	diff = str(t_diff)		
-	
-	if diff[2] == 'd':
-		if diff[5] == 's':
-			if diff[9].isdigit():
-				number_of_bins = int(diff[0])*6 + int(int(diff[8]+diff[9])/4) + 1
-			else:
-				number_of_bins = int(diff[0])*6 + int(int(diff[8])/4) + 1	
-		else:
-			if diff[8].isdigit():
-				number_of_bins = int(diff[0])*6 + int(int(diff[7]+diff[8])/4) + 1
-			else:
-				number_of_bins = int(diff[0])*6 + int(int(diff[7])/4) + 1	
-		
-	elif diff[3] == 'd':
-		if diff[10].isdigit():		
-			number_of_bins = int(diff[0]+diff[1])*6 + int(int(diff[9]+diff[10])/4) + 1
-		else:
-			number_of_bins = int(diff[0]+diff[1])*6 + int(int(diff[9])/4) + 1
-		
-	elif diff[4] == 'd':
-		if diff[11].isdigit():
-			number_of_bins = int(diff[0]+diff[1]+diff[2])*6 + int(int(diff[10]+diff[11])/4) + 1
-		else:
-			number_of_bins = int(diff[0]+diff[1]+diff[2])*6 + int(int(diff[10])/4) + 1
-		
-	elif diff[0] == '0':
-		number_of_bins = 1
-		
-	else:
-		number_of_bins = int(int(diff[0]+diff[1])/4) + 1
-		
-	#print number_of_bins
-	#print sayac
-		
+        #find number of bins using time difference
+        t_diff = upper_date-base_date
+        print t_diff
+        diff = str(t_diff)		
+        
+        if diff[2] == 'd':
+            if diff[5] == 's':
+                if diff[9].isdigit():
+                    number_of_bins = int(diff[0])*6 + int(int(diff[8]+diff[9])/4) + 1
+                else:
+                    number_of_bins = int(diff[0])*6 + int(int(diff[8])/4) + 1	
+            else:
+                if diff[8].isdigit():
+                    number_of_bins = int(diff[0])*6 + int(int(diff[7]+diff[8])/4) + 1
+                else:
+                    number_of_bins = int(diff[0])*6 + int(int(diff[7])/4) + 1	
+            
+        elif diff[3] == 'd':
+            if diff[10].isdigit():		
+                number_of_bins = int(diff[0]+diff[1])*6 + int(int(diff[9]+diff[10])/4) + 1
+            else:
+                number_of_bins = int(diff[0]+diff[1])*6 + int(int(diff[9])/4) + 1
+            
+        elif diff[4] == 'd':
+            if diff[11].isdigit():
+                number_of_bins = int(diff[0]+diff[1]+diff[2])*6 + int(int(diff[10]+diff[11])/4) + 1
+            else:
+                number_of_bins = int(diff[0]+diff[1]+diff[2])*6 + int(int(diff[10])/4) + 1
+            
+        elif diff[0] == '0':
+            number_of_bins = 1
+            
+        else:
+            number_of_bins = int(int(diff[0]+diff[1])/4) + 1
+            
+        #print number_of_bins
+        #print sayac
+            
 
-	#create data structure to hold tweet bins
-	tweet_bins = [[] for i in range(number_of_bins)]
+        #create data structure to hold tweet bins
+        tweet_bins = [[] for i in range(number_of_bins)]
 
-	
-	#assign tweets to correct bin
-sayac2 = 0
-f.close()
-with open('tweets.jsonl') as g:
-	for row in g:
-		tweet = json.loads(row)
-		
-		#print sayac2
+        
+        #assign tweets to correct bin
+    sayac2 = 0
+    f.close()
+    with open(input_file) as g:
+        for row in g:
+            tweet = json.loads(row)
+            
+            #print sayac2
 
-		temp_date = tweet['_source']['created_at']
-		#print temp_date
-			
-		temp_date = temp_date.split(" ")
-		temp_year = int(temp_date[5])
-			
-		if temp_date[1] == "Jan":
-			temp_month = 1
-		if temp_date[1] == "Feb":
-			temp_month = 2
-		if temp_date[1] == "Mar":
-			temp_month = 3
-		if temp_date[1] == "Apr":
-			temp_month = 4
-		if temp_date[1] == "May":
-			temp_month = 5
-		if temp_date[1] == "Jun":
-			temp_month = 6
-		if temp_date[1] == "Jul":
-			temp_month = 7
-		if temp_date[1] == "Aug":
-			temp_month = 8
-		if temp_date[1] == "Sep":
-			temp_month = 9
-		if temp_date[1] == "Oct":
-			temp_month = 10
-		if temp_date[1] == "Nov":
-			temp_month = 11
-		if temp_date[1] == "Dec":
-			temp_month = 12	
-			
+            temp_date = tweet['_source']['created_at']
+            #print temp_date
+                
+            temp_date = temp_date.split(" ")
+            temp_year = int(temp_date[5])
+                
+            if temp_date[1] == "Jan":
+                temp_month = 1
+            if temp_date[1] == "Feb":
+                temp_month = 2
+            if temp_date[1] == "Mar":
+                temp_month = 3
+            if temp_date[1] == "Apr":
+                temp_month = 4
+            if temp_date[1] == "May":
+                temp_month = 5
+            if temp_date[1] == "Jun":
+                temp_month = 6
+            if temp_date[1] == "Jul":
+                temp_month = 7
+            if temp_date[1] == "Aug":
+                temp_month = 8
+            if temp_date[1] == "Sep":
+                temp_month = 9
+            if temp_date[1] == "Oct":
+                temp_month = 10
+            if temp_date[1] == "Nov":
+                temp_month = 11
+            if temp_date[1] == "Dec":
+                temp_month = 12	
+                
 
-		temp_day = int(temp_date[2])
+            temp_day = int(temp_date[2])
 
-		temp_hour = int(temp_date[3][0] + temp_date[3][1])
-		temp_min = int(temp_date[3][3] + temp_date[3][4])
-		temp_sec = int(temp_date[3][6] + temp_date[3][7]) 
+            temp_hour = int(temp_date[3][0] + temp_date[3][1])
+            temp_min = int(temp_date[3][3] + temp_date[3][4])
+            temp_sec = int(temp_date[3][6] + temp_date[3][7]) 
 
-		date = datetime.datetime(temp_year,temp_month,temp_day,temp_hour,temp_min,temp_sec)
-		
-		#print date
-		count = base_date
-		bin_number = -1		
-		for i in range(0,number_of_bins):
-			if count > date:
-				break
+            date = datetime.datetime(temp_year,temp_month,temp_day,temp_hour,temp_min,temp_sec)
+            
+            #print date
+            count = base_date
+            bin_number = -1		
+            for i in range(0,number_of_bins):
+                if count > date:
+                    break
 
-			else:
-				count = count + datetime.timedelta(hours=4)
-				bin_number = bin_number + 1
+                else:
+                    count = count + datetime.timedelta(hours=4)
+                    bin_number = bin_number + 1
 
-		#print bin_number
-		
-		tweet_bins[bin_number].append(tweet)
-		
-		sayac2 = sayac2 + 1
-	
-
-
-#evaluate the bins and collect the score
-score_pos = [[] for i in range(number_of_bins)]
-score_neg = [[] for i in range(number_of_bins)]
-sentiment.main()
-tweet_count = 0
-for j in range(0,number_of_bins):
-	pos_count = 0
-	neg_count = 0		
-	for k in range(0, len(tweet_bins[j])):
-		#print j
-		#print k
-		#print neg_count	
-		#print pos_count
-		
-		result = sentiment.test(tweet_bins[j][k]['_source']['text'])
-		if result == 'negative':
-			neg_count = neg_count + 1
-		if result == 'positive':
-			pos_count = pos_count + 1
-		tweet_count = tweet_count + 1		
-		
-	score_pos[j] = pos_count
-	score_neg[j] = neg_count
-	#print j
-
-#print score_pos	
-#print score_neg
-	
-
-#drawplot
+            #print bin_number
+            
+            tweet_bins[bin_number].append(tweet)
+            
+            sayac2 = sayac2 + 1
+        
 
 
-index = []
-for h in range(0, len(score_pos)):
-	index.append(h)
+    #evaluate the bins and collect the score
+    score_pos = [[] for i in range(number_of_bins)]
+    score_neg = [[] for i in range(number_of_bins)]
+    tweet_count = 0
+    for j in range(0,number_of_bins):
+        pos_count = 0
+        neg_count = 0		
+        for k in range(0, len(tweet_bins[j])):
+            #print j
+            #print k
+            #print neg_count	
+            #print pos_count
+            
+            result = sentiment.classify(classifier, tweet_bins[j][k]['_source']['text'])
+            if result == 'negative':
+                neg_count = neg_count + 1
+            if result == 'positive':
+                pos_count = pos_count + 1
+            tweet_count = tweet_count + 1		
+            
+        score_pos[j] = pos_count
+        score_neg[j] = neg_count
+        print 'bin', j, 'positive', score_pos[j], 'negative', score_neg[j]
 
-plt.plot(index,score_pos, 'g', label='positive', linewidth=4.0)
-plt.plot(index,score_neg, 'r', label='negative', linewidth=4.0)
+    #print score_pos	
+    #print score_neg
 
-plt.legend()
+    return input_file, score_pos, score_neg
 
-plt.show()
-	
+
+def draw(score_pos, score_neg):
+    #drawplot
+    index = []
+    for h in range(0, len(score_pos)):
+        index.append(h)
+
+    plt.plot(index,score_pos, 'g', label='positive', linewidth=4.0)
+    plt.plot(index,score_neg, 'r', label='negative', linewidth=4.0)
+    plt.legend()
+    plt.show()
+
+
+@click.command()
+@click.argument('tweets-dir', type=click.Path(file_okay=False))
+@click.argument('classifier', type=click.File('r'))
+@click.option('--output-file', '-o', type=click.File('w'), default='features.jsonl')
+def main(tweets_dir, classifier, output_file):
+    classifier = pickle.load(classifier)
+
+    tasks = (
+        (os.path.join(tweets_dir, fname), classifier) for fname in os.listdir(tweets_dir)
+    )
+
+    pool = mp.Pool()
+    try:
+        for fname, score_pos, score_neg in pool.imap(get_sentiment, tasks):
+            json.dump((fname, score_pos, score_neg), output_file)
+            output_file.write('\n')
+    finally:
+        pool.close()
+
+
+if __name__ == '__main__':
+    main()
--- classifier/sentiment.py
+++ classifier/sentiment.py
@@ -1,12 +1,11 @@
 # http://www.laurentluce.com/posts/twitter-sentiment-analysis-using-python-and-nltk/
+import functools
 import sys
 import nltk
 from positive import pos_sent
 from negative import neg_sent
+import click
 #from test import test_set
-
-tweets = []
-
 
 
 def get_words_in_tweets(tweets):
@@ -15,53 +14,49 @@ def get_words_in_tweets(tweets):
         all_words.extend(words)
     return all_words
 
+
 def get_word_features(wordlist):
     wordlist = nltk.FreqDist(wordlist)
     # print wordlist.most_common(10)
     word_features = wordlist.keys()
     return word_features
 
-word_features = get_word_features(get_words_in_tweets(tweets))
 
-def extract_features(document):
+def extract_features(document, word_features):
     document_words = set(document)
     features = {}
     for word in word_features:
         features['contains(%s)' % word] = (word in document_words)
     return features
 
-training_set = nltk.classify.apply_features(extract_features, tweets)
 
-def main():
-    global tweets
-    #tweets = tweet
+def classify((word_features, classifier), tweet):
+    return classifier.classify(extract_features(tweet.split(), word_features))
 
-    
+
+@click.command()
+@click.argument('output-file', type=click.File('w'))
+def main(output_file):
+    tweets = []
     for (words, sentiment) in pos_sent + neg_sent:
         words_filtered = [e.lower() for e in words.split() if len(e) >= 3] 
         print words_filtered
         tweets.append((words_filtered, sentiment))
 
-    
-    global word_features
     word_features = get_word_features(get_words_in_tweets(tweets))
 
-    
-    global training_set
-    training_set = nltk.classify.apply_features(extract_features, tweets)
+    training_set = nltk.classify.apply_features(
+        functools.partial(extract_features, word_features),
+        tweets
+    )
     
     print 'training'
-    global classifier 
     classifier = nltk.NaiveBayesClassifier.train(training_set)
     #print(nltk.classify.accuracy(classifier, test_set))
 
     import pickle
-    with open('../dev/sentiment-classifier.pkl', 'w') as f:
-        pickle.dump(classifier, f)
+    pickle.dump((word_features, classifier), output_file)
     print 'saved'
-    
-def test(tweet):
-    return classifier.classify(extract_features(tweet.split()))
 
 
 if __name__ == '__main__':
